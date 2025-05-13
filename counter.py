c959648758800dfb0416aa48b8695b8aff241d0d
evgeniavolkova <evgeniafil04@gmail.com>
2025-05-13 23:33:06 +0500

    basic

--- counter.py
+++ counter.py
@@ -1,1 +1,14 @@
-print(3)
+from aiohttp import web
+
+visit_count = 0
+
+async def handle(request):
+    global visit_count
+    visit_count += 1
+    return web.Response(text=f'Hello World! Visit count: {visit_count}')
+
+app = web.Application()
+app.router.add_get('/', handle)
+
+if __name__ == '__main__':
+    web.run_app(app, port=8080)
